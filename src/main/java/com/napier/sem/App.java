1adb397e1b6cdcc7a647b3386e64e40e024d2883
careykva <40592376@live.napier.ac.uk>
2022-02-09 22:50:54 -0400

    Boo Yah

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -9,21 +9,15 @@ public class App
 {
     public static void main(String[] args)
     {
-        // Connect to MongoDB on local system - we're using port 27000
-        MongoClient mongoClient = new MongoClient("localhost", 27000);
-        // Get a database - will create when we use it
+        MongoClient mongoClient = new MongoClient("localhost", 27017);
         MongoDatabase database = mongoClient.getDatabase("mydb");
-        // Get a collection from the database
         MongoCollection<Document> collection = database.getCollection("test");
-        // Create a document to store
         Document doc = new Document("name", "Kevin Sim")
                 .append("class", "Software Engineering Methods")
                 .append("year", "2021")
                 .append("result", new Document("CW", 95).append("EX", 85));
-        // Add document to collection
         collection.insertOne(doc);
 
-        // Check document in collection
         Document myDoc = collection.find().first();
         System.out.println(myDoc.toJson());
         System.out.println("Boo yah!");
